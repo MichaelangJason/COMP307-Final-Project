d8b7afd8a2a78cb0c5e6ec996f39391ddb22b8c5
Han Wen Fu <fuhanwen2002@icloud.com>
2024-11-28 17:07:52 -0500

    Add navigate back option to RedButtonLink

--- frontend/src/components/RedButtonLink.tsx
+++ frontend/src/components/RedButtonLink.tsx
@@ -1,19 +1,30 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCaretLeft } from "@fortawesome/free-solid-svg-icons";
 
 import "../styles/RedButtonLink.scss";
 
 interface Props {
-  pageTo: string;
+  pageTo?: string;
   text: string;
-  arrow?: boolean;
+  goBack?: boolean;
 }
 
-const RedButtonLink = ({ pageTo, text, arrow = false }: Props) => {
+const RedButtonLink = ({ pageTo = "/", text, goBack = false }: Props) => {
+  const navigate = useNavigate();
+
+  const handleClick = (e: React.MouseEvent) => {
+    e.preventDefault();
+    navigate(-1);
+  };
+
   return (
-    <Link className="redButton" to={pageTo}>
-      {arrow && <FontAwesomeIcon icon={faCaretLeft} />}
+    <Link
+      className="redButton"
+      to={pageTo}
+      onClick={goBack ? handleClick : undefined}
+    >
+      {goBack && <FontAwesomeIcon icon={faCaretLeft} />}
       {text}
     </Link>
   );
