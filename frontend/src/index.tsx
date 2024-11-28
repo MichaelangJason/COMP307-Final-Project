181cfbbc79b8c278dc44e1506d1c96be3da2f77a
Han Wen Fu <fuhanwen2002@icloud.com>
2024-11-28 17:10:00 -0500

    Fix warning

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { here } from "@shared/api/here";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
--- frontend/src/layouts/NavBar.tsx
+++ frontend/src/layouts/NavBar.tsx
@@ -1,4 +1,4 @@
-import { Link, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 import logo from "../images/logo.png";
 import RedButtonLink from "../components/RedButtonLink";
