8fac8fc15c71535d4a71ccfaa06a6e37bb2c4870
Han Wen Fu <fuhanwen2002@icloud.com>
2024-11-28 16:59:21 -0500

    Add arrow to RedButtonLink.tsx

--- frontend/src/components/RedButtonLink.tsx
+++ frontend/src/components/RedButtonLink.tsx
@@ -1,15 +1,19 @@
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretLeft } from "@fortawesome/free-solid-svg-icons";
 
 import "../styles/RedButtonLink.scss";
 
 interface Props {
   pageTo: string;
   text: string;
+  arrow?: boolean;
 }
 
-const RedButtonLink = ({ pageTo, text }: Props) => {
+const RedButtonLink = ({ pageTo, text, arrow = false }: Props) => {
   return (
     <Link className="redButton" to={pageTo}>
+      {arrow && <FontAwesomeIcon icon={faCaretLeft} />}
       {text}
     </Link>
   );
