eaf6fb459495aaecff057d3cdc03152296b5eecd
mkljs <mkl.js@outlook.com>
2024-12-02 11:02:34 -0500

    feat: schemas and jest - set-schemas now also check if there exists a MongoDB instance - we introduce jest for transpiled js file under /dist/tests   - they will be used to test insert/delete/update

--- backend/schemas/set-schemas.sh
+++ backend/schemas/set-schemas.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+# Check if mongosh is installed
 if ! command -v mongosh &> /dev/null
 then
     echo "mongosh could not be found"
-    exit
+    exit 1
 fi
 
+# Check if MongoDB is running
+if ! mongosh --eval "db.adminCommand('ping')" &> /dev/null
+then
+    echo "MongoDB is not running"
+    exit 1
+fi
+
+# If all checks pass, proceed with schema creation
 mongosh "./user.js" # create user schema / collection
 mongosh "./meeting.js" # create meeting schema / collection
 mongosh "./poll.js" # create poll schema / collection
---
+++ backend/src/tests/db/insert.test.ts
@@ -0,0 +1,9 @@
+console.log("Hello Jest");
+
+function sum(a: number, b: number) {
+  return a + b;
+}
+
+test("sum", () => {
+  expect(sum(1, 2)).toBe(3);
+});
