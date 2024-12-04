e3b0ec0308e96d65a897671be117751618f1a801
jsle-stack <jessicalee467@gmail.com>
2024-12-04 14:47:35 -0500

    Create Meeting Card Component

---
+++ frontend/src/components/meetings/MeetingCard.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import bin_icon from "../../images/bin.png";
+import clock_icon from "../../images/clock.png";
+import location_icon from "../../images/location.png";
+import person_icon from "../../images/person.png";
+import "../../styles/MeetingCard.scss";
+
+interface Props {
+  title: string;
+  status: string;
+  dateTime: string; //Expected format: 2024-11-18, 15:30 - 18:00
+  location: string;
+  person: string;
+}
+
+const MeetingCard = ({ title, status, dateTime, location, person }: Props) => {
+  return (
+    <div className="meetingCard">
+      <div className="card-title">
+        <h2>{title}</h2>
+        <button>
+          <img src={bin_icon} alt="Bin Icon"></img>
+        </button>
+      </div>
+
+      <div className={`card-status ${status.toLowerCase()}`}>{status}</div>
+
+      <div className="card-description">
+        <div className="card-info">
+          <img src={clock_icon} alt="Clock Icon" />
+          {dateTime}
+        </div>
+        <div className="card-info">
+          <img src={location_icon} alt="Location Icon" />
+          {location}
+        </div>
+        <div className="card-info">
+          <img src={person_icon} alt="Person Icon" />
+          {person}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MeetingCard;
--- frontend/src/private-pages/Meetings.tsx
+++ frontend/src/private-pages/Meetings.tsx
@@ -1,5 +1,18 @@
-const Meetings = () => {
-  return <>Meetings</>;
+import MeetingCard from "../components/meetings/MeetingCard";
+
+const Create = () => {
+  return (
+    <>
+      <MeetingCard
+        title="COMP 307 Office Hours"
+        status="Closed"
+        dateTime="2024-11-18, 15:30 - 18:00"
+        location="MC24"
+        person="Me"
+      ></MeetingCard>
+      <div></div>
+    </>
+  );
 };
 
-export default Meetings;
+export default Create;
