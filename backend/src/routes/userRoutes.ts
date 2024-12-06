2bdd35be493cdac1006c2d0002ae45d913523983
Felicia Wei <85764178+FeliciaW05@users.noreply.github.com>
2024-12-06 02:23:42 -0500

    feat: Profile structure

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,11 +1,10 @@
 import express, { Request, Response } from 'express';
-import userController from '../controller/userController';
+import userController from '../controller/userController.js';
 
 const userRouter = express.Router();
 
 // Define routes and associate them with controller methods
-userRouter.post('/register', async (req: Request, res: Response) => await userController.register(req, res));
-userRouter.post('/login', async (req: Request, res: Response) => await userController.login(req, res));
-userRouter.delete('/logout', async (req: Request, res: Response) => await userController.logout(req, res));
+userRouter.get('/profile', async (req: Request, res: Response) => await userController.getProfile(req, res));
+userRouter.put('/profile', async (req: Request, res: Response) => await userController.updateProfile(req, res));
 
 export default userRouter;
