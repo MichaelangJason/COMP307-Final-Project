72d0c70acf4c97ca916570aa28b04b328f1fb23e
jsle-stack <jessicalee467@gmail.com>
2024-12-07 13:02:23 -0500

    Add Welcome Message on the Private NavBar

--- frontend/src/layouts/NavBarContent.tsx
+++ frontend/src/layouts/NavBarContent.tsx
@@ -5,12 +5,14 @@ import "../styles/NavBarContent.scss";
 interface Props {
   pageTo: string;
   text: string;
+  userEmail?: string | null; // New attribute to display msg "Welcome userEmail!"
 }
 
-const NavBarContent = ({ pageTo, text }: Props) => {
+const NavBarContent = ({ pageTo, text, userEmail }: Props) => {
   return (
     <div id="navbar">
       <img id="logo" src={logo} alt="Logo" />
+      {userEmail && <span>Welcome {userEmail}!</span>}
       <RedButtonLink pageTo={pageTo} text={text} />
     </div>
   );
--- frontend/src/layouts/PrivateNav.tsx
+++ frontend/src/layouts/PrivateNav.tsx
@@ -2,10 +2,35 @@ import { Link, Outlet } from "react-router-dom";
 import NavBarContent from "./NavBarContent";
 import "../styles/PrivateNav.scss";
 import { FaBars, FaTimes } from "react-icons/fa";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const PrivateNav = () => {
   const [isNavVisible, setIsNavVisible] = useState(false);
+  // Welcome message
+  const [userEmail, setUserEmail] = useState<string | null>(null);
+
+  // TODO - backend :  to query the userEmail / username
+  useEffect(() => {
+    // Simulating a backend API call to fetch user details
+    const fetchUserData = async () => {
+      try {
+        // const response = await fetch("/api/user");
+        // const data = await response.json();
+
+        // Dummy example
+        const data = {
+          email: "user@example.com",
+          name: "John Doe",
+        };
+
+        setUserEmail(data.email); // Assuming the backend returns { email: "user@example.com" }
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
+
+    fetchUserData();
+  }, []);
 
   const toggleNavBar = () => {
     setIsNavVisible((prev) => !prev);
@@ -13,7 +38,7 @@ const PrivateNav = () => {
 
   return (
     <>
-      <NavBarContent pageTo="/" text="Log Out" />
+      <NavBarContent pageTo="/" text="Log Out" userEmail={userEmail} />
       <div id="privateSeparator">
         <nav className={isNavVisible ? "responsive_nav" : ""}>
           <Link to="/user/:id" onClick={toggleNavBar}>
