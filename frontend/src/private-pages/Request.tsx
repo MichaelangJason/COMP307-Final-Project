fb7f4eff76313a57bb7503ee565a3e27af35b0de
Han Wen Fu <fuhanwen2002@icloud.com>
2024-12-13 14:37:38 -0500

    Add filter to requests

--- frontend/src/private-pages/Request.tsx
+++ frontend/src/private-pages/Request.tsx
@@ -3,56 +3,89 @@ import { useState } from "react";
 import RequestAccordion from "components/RequestAccordion";
 import "../styles/Request.scss";
 
+interface RequestAccordion {
+  status: "expired" | "pending" | "accepted";
+  name: string;
+  email: string;
+  date: string;
+  time: string;
+  reason: string;
+  showButtons: boolean;
+}
+
 const Request = () => {
-  const [filter, setFilter] = useState("All");
+  const [filter, setFilter] = useState<"all" | RequestAccordion["status"]>(
+    "all"
+  );
+
+  const requestAccordions: RequestAccordion[] = [
+    {
+      status: "pending",
+      name: "Eve Zhang",
+      email: "jiaju.nie@mail.mcgill.ca",
+      date: "2024-11-30",
+      time: "14:30",
+      reason: "bla bla bla bla bla bla bla",
+      showButtons: false,
+    },
+    {
+      status: "pending",
+      name: "Yes Madam",
+      email: "jiaju.nie@mail.mcgill.ca",
+      date: "2024-11-30",
+      time: "14:30",
+      reason:
+        "I think I’ve seen this film before, but I didn’t like the ending. Hey hey you you I dont like your girlfriend, no why no why I think you need a new one.",
+      showButtons: true,
+    },
+    {
+      status: "accepted",
+      name: "Halo You",
+      email: "jiaju.nie@mail.mcgill.ca",
+      date: "2024-11-30",
+      time: "14:30",
+      reason: "bla bla bla bla bla bla bla",
+      showButtons: false,
+    },
+    {
+      status: "expired",
+      name: "Yes Madam",
+      email: "jiaju.nie@mail.mcgill.ca",
+      date: "2024-11-30",
+      time: "14:30",
+      reason:
+        "bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla",
+      showButtons: true,
+    },
+  ];
+
   return (
     <>
       <h1>Manage Requests</h1>
       <div id="request">
-        <RequestAccordion
-          status="pending"
-          name="Eve Zhang"
-          email="jiaju.nie@mail.mcgill.ca"
-          date="2024-11-30"
-          time="14:30"
-          reason="bla bla bla bla bla bla bla"
-        />
-        <RequestAccordion
-          status="pending"
-          name="Yes Madam"
-          email="jiaju.nie@mail.mcgill.ca"
-          date="2024-11-30"
-          time="14:30"
-          reason="I think I’ve seen this film before, but I didn’t like the ending. Hey
-          hey you you I dont like your girfriend, no why no why I think you need
-          a new one."
-          showButtons
-        />
-        <RequestAccordion
-          status="accepted"
-          name="Halo You"
-          email="jiaju.nie@mail.mcgill.ca"
-          date="2024-11-30"
-          time="14:30"
-          reason="bla bla bla bla bla bla bla"
-        />
-        <RequestAccordion
-          status="expired"
-          name="Yes Madam"
-          email="jiaju.nie@mail.mcgill.ca"
-          date="2024-11-30"
-          time="14:30"
-          reason="bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla"
-          showButtons
-        />
+        <div className="accordions">
+          {requestAccordions
+            .filter((accordion) => filter === "all" || accordion.status === filter)
+            .map((data) => (
+              <RequestAccordion
+                status={data.status}
+                name={data.name}
+                email={data.email}
+                date={data.date}
+                time={data.time}
+                reason={data.reason}
+                showButtons={data.showButtons}
+              />
+            ))}
+        </div>
         <div className="filter-options">
           <label>
             <input
               type="radio"
               name="filter"
               value="All"
-              checked={filter === "All"}
-              onChange={() => setFilter("All")}
+              checked={filter === "all"}
+              onChange={() => setFilter("all")}
             />
             All
           </label>
@@ -60,41 +93,31 @@ const Request = () => {
             <input
               type="radio"
               name="filter"
-              value="Upcoming"
-              checked={filter === "Upcoming"}
-              onChange={() => setFilter("Upcoming")}
+              value="Expired"
+              checked={filter === "expired"}
+              onChange={() => setFilter("expired")}
             />
-            Upcoming
+            Expired
           </label>
           <label>
             <input
               type="radio"
               name="filter"
-              value="Live"
-              checked={filter === "Live"}
-              onChange={() => setFilter("Live")}
+              value="Pending"
+              checked={filter === "pending"}
+              onChange={() => setFilter("pending")}
             />
-            Live
+            Pending
           </label>
           <label>
             <input
               type="radio"
               name="filter"
-              value="Closed"
-              checked={filter === "Closed"}
-              onChange={() => setFilter("Closed")}
+              value="Accepted"
+              checked={filter === "accepted"}
+              onChange={() => setFilter("accepted")}
             />
-            Closed
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="filter"
-              value="Canceled"
-              checked={filter === "Canceled"}
-              onChange={() => setFilter("Canceled")}
-            />
-            Canceled
+            Accepted
           </label>
         </div>
       </div>
