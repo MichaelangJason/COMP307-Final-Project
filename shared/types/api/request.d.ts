a74a565d61d1fe686117d805461e5a8764b7abb2
mkljs <mkl.js@outlook.com>
2024-12-13 20:57:19 -0500

    feat: poll Response/Request types

--- shared/types/api/request.d.ts
+++ shared/types/api/request.d.ts
@@ -1,13 +1,9 @@
 import { RequestStatus } from './common';
 
+// common
 export interface RequestParams {
   requestId: string; // host id (for creating request), user id (for managing request)
 }
-
-export interface RequestUpdateBody {
-  status: RequestStatus; // can only update status
-}
-
 export interface RequestInfo {
   proposerInfo: {
     firstName: string;
@@ -26,7 +22,14 @@ export interface RequestInfoResponse extends RequestInfo {
   status: RequestStatus;
 }
 
+// create request
 export interface RequestCreateBody extends RequestInfo {
   hostId: string; // host id (for creating request)
 }
+
+// update request
+export interface RequestUpdateBody {
+  status: RequestStatus; // can only update status
+}
+
 
