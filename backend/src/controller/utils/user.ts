52073bd29dfc36937c00cdd4b1a432e7268eade7
mkljs <mkl.js@outlook.com>
2024-12-17 16:14:53 -0500

    fix: bypass auth also bypass isallowed

--- backend/src/controller/utils/user.ts
+++ backend/src/controller/utils/user.ts
@@ -1,7 +1,7 @@
 import { UserRole } from "../../utils/statusEnum";
 
 export const isAllowed = (role: UserRole, paramsID: any, userID: any) => {
-  return Number(process.env.DEV_MODE) || 
-    role == UserRole.ADMIN || 
+  return  role == UserRole.ADMIN || 
+    Number(process.env.BYPASS_AUTH) ||
     paramsID == userID;
 }
